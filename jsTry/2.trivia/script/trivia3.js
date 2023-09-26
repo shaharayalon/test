238e024f4a915c90d5a878228fed7387d2a46d0b
shaharayalon <136680992+shaharayalon@users.noreply.github.com>
2023-09-26 22:36:16 +0300

    raziel

--- jsTry/2.trivia/script/trivia3.js
+++ jsTry/2.trivia/script/trivia3.js
@@ -488,6 +488,7 @@ window.addEventListener("load", () => {
         gameOverWin.style.display = "none";
       }
       if (playersRate.length) {
+        console.log("raziel");
         for (let i = 0; i < playersRate.length; i++) {
           if (playersRate[i].correctAnswers == you.correctAnswers) {
             if (playersRate[i].totalTime > you.totalTime) {
